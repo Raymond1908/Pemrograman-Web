972504f3856d5ab2091bf2c959160227e4eeb717
ariellembong <135602830+ariellembong@users.noreply.github.com>
2023-06-06 19:31:31 +0800

    Comment penjelasan

--- Pemrograman Web/login/halamanlogin.php
+++ Pemrograman Web/login/halamanlogin.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if ( isset($_SESSION["login"]) ) {
+if ( isset($_SESSION["login"]) ) { //memaksa user pergi ke chatbubble.php jika user belum log out
   header("Location: chatbubble.php");
   exit;
 }
